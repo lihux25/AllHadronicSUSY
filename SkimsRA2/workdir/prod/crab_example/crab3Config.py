56bb8ab88fff2044ba75a42839802c40cb622438
Hongxuan <Hongxuan_Liu@baylor.edu>
2014-12-12 16:54:08 -0600

    crab3 syntax change...

--- SkimsRA2/workdir/prod/crab_example/crab3Config.py
+++ SkimsRA2/workdir/prod/crab_example/crab3Config.py
@@ -11,13 +11,13 @@ config.JobType.allowNonProductionCMSSW = False
 
 config.section_("Data")
 config.Data.inputDataset = '/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Phys14DR-PU20bx25_PHYS14_25_V1-v1/MINIAODSIM'
-config.Data.dbsUrl = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 5
 config.Data.publication = False
-config.Data.publishDbsUrl = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
+config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/'
 config.Data.publishDataName = 'PHYS14_PU20bx25_PHYS14_25_V1-FLAT'
-config.Data.outlfn = '/store/user/lpcsusyhad/PHYS14/PU20bx25_TTJets_MSDecaysCKM_madgraph-tauola/'
+config.Data.outLFN = '/store/user/lpcsusyhad/PHYS14/PU20bx25_TTJets_MSDecaysCKM_madgraph-tauola/'
 
 config.Data.ignoreLocality = False
 
